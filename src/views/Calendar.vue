9adb7f1e84c10112527cb3529b2ebb34541ba38e
Yuxiu <no>
2021-07-05 00:50:00 +0800

    Add Calendar View

---
+++ src/views/Calendar.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-md-center">
+            <div class="col-sm-auto col-md-4">
+            Calender Header
+            </div>
+            <div class="col-sm-auto col-md-8">
+            Calender Body
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Calender',
+});
+</script>
