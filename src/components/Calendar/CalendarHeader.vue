233a51297ea01708a979989f153bfd40fba0a6ed
Yuxiu <no>
2021-07-06 03:36:25 +0800

    Add Calendar Header

---
+++ src/components/Calendar/CalendarHeader.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="ratio ratio-1x1">
+        <img alt="Tree House" src="@/assets/TreeHouse.jpg">
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'CalenderHeader',
+});
+</script>
+
+<style lang="sass" scoped>
+  img
+    max-height: 100%
+    max-width: 100%
+    width: auto
+    height: auto
+    margin: auto
+    right: 0
+    bottom: 0
+</style>
--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row justify-content-md-center">
             <div class="col-sm-auto col-md-4">
-            Calender Header
+            <calendar-header/>
             </div>
             <div class="col-sm-auto col-md-8">
             <calendar-body/>
@@ -13,11 +13,13 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import CalendarHeader from '../components/Calendar/CalendarHeader.vue';
 import CalendarBody from '../components/Calendar/CalendarBody.vue';
 
 export default defineComponent({
   name: 'Calender',
   components: {
+    CalendarHeader,
     CalendarBody,
   },
 });
