7bb3a4105790b0ddc2c4d820596c4d4416a0117f
Yuxiu <no>
2021-07-14 00:15:00 +0800

    Format

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'max-len': ["error", { "code": 200 }],
-    "linebreak-style": ["error", "windows"],
+    'max-len': ['error', { 'code': 200 }],
+    'linebreak-style': ['error', 'windows'],
   },
 };
