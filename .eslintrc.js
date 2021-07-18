9016b9dec9bffbd85f61421c51200262d67d6807
Yuxiu <no>
2021-07-18 22:20:27 +0800

    Add EsLint Rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
   },
   rules: {
     'arrow-parens': 'off',
+    'no-confusing-arrow': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-param-reassign': 'off',
