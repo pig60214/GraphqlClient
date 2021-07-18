e2f1f2a012f50cc2140e093a4823c93db36d1d10
Yuxiu <no>
2021-07-18 11:12:39 +0800

    Apollo Client useQuery() Need To Used In setup()

--- src/class/MonthOfCalendar.ts
+++ src/class/MonthOfCalendar.ts
@@ -1,13 +1,85 @@
+import DateOfCalendar from './DateOfCalendar';
+
 export default class MonthOfCalendar {
   year: number
 
   month: number
+
+  get firstDate(): Date {
+    return new Date(this.year, this.month, 1);
+  }
+
+  get lastDate(): Date {
+    return new Date(this.year, this.month + 1, 0);
+  }
+
+  get firstDateString(): string {
+    return `${this.firstDate.getFullYear()}/${this.firstDate.getMonth() + 1}/${this.firstDate.getDate()}`;
+  }
+
+  get lastDateString(): string {
+    return `${this.lastDate.getFullYear()}/${this.lastDate.getMonth() + 1}/${this.lastDate.getDate()}`;
+  }
+
+  get dateOfCalendarList(): DateOfCalendar[] {
+    const dateList = new Array<DateOfCalendar>();
+    const date = this.firstDate;
+
+    this.addEmptyDateOfCalendarAtBeginning(date, dateList);
+
+    while (date.getMonth() === this.month) {
+      const dateOfCalendar = new DateOfCalendar();
+      const newDate = new Date(date.getTime());
+      dateOfCalendar.date = newDate;
+
+      dateList.push(dateOfCalendar);
+
+      date.setDate(date.getDate() + 1);
+    }
+
+    this.addEmptyDateOfCalendarAtEnd(date, dateList);
+    return dateList;
+  }
+
+  get calendar(): DateOfCalendar[][] {
+    const weekList = new Array<Array<DateOfCalendar>>();
+
+    let start = 0;
+    while (start < this.dateOfCalendarList.length) {
+      weekList.push(this.dateOfCalendarList.slice(start, start + 7));
+      start += 7;
+    }
+    console.log(this.dateOfCalendarList);
+    return weekList;
+  }
 
   addMonth(month : number) : void {
     const date = new Date(this.year, this.month, 1);
     date.setMonth(date.getMonth() + month);
     this.year = date.getFullYear();
     this.month = date.getMonth();
+  }
+
+  private addEmptyDateOfCalendarAtBeginning = (startDate: Date, dateList: Array<DateOfCalendar>) => {
+    if (startDate.getDay() !== 0) {
+      let count = startDate.getDay();
+      while (count > 0) {
+        const nullDateOfCalener = new DateOfCalendar();
+        dateList.push(nullDateOfCalener);
+        count -= 1;
+      }
+    }
+  }
+
+  private addEmptyDateOfCalendarAtEnd = (startDate: Date, dateList: Array<DateOfCalendar>) => {
+    if (startDate.getDay() !== 6) {
+      let count = startDate.getDay();
+      while (count < 7) {
+        const nullDateOfCalener = new DateOfCalendar();
+        dateList.push(nullDateOfCalener);
+        count += 1;
+      }
+    }
   }
 
   constructor(year: number, month: number) {
--- src/components/Calendar/CalendarBodyDates.vue
+++ src/components/Calendar/CalendarBodyDates.vue
@@ -4,20 +4,23 @@
     <div class="col px-0 w-1-out-of-7" v-for="day, index in dayList" :key="index">{{ day }}</div>
   </div>
   <div class="row" v-for="week, index in weekListOfCurrentMonth" :key="index">
-    <calendar-body-date v-for="dateOfCalendar, indexOfDate in week" :key="indexOfDate" :dateOfCalendar="dateOfCalendar"/>
+    <calendar-body-date v-for="dateOfCalendar, indexOfDate in week" :key="indexOfDate" :dateOfCalendar="getPosts(dateOfCalendar)"/>
   </div>
 </div>
 </template>
 
 <script lang="ts">
 import {
-  computed,
   defineComponent,
   reactive,
+  ref,
+  watch,
 } from 'vue';
 import DateOfCalendar from '@/class/DateOfCalendar';
 import { useStore } from '@/store';
-import useCalendar from '@/composables/useCalendar';
+import Post from '@/interface/Post';
+import { useQuery, useResult } from '@vue/apollo-composable';
+import gql from 'graphql-tag';
 import CalendarBodyDate from './CalendarBodyDate.vue';
 
 export default defineComponent({
@@ -25,17 +28,63 @@ export default defineComponent({
   name: 'CalendarBodyDates',
   setup() {
     const store = useStore();
-    const { dateListOfCurrentMonth, weekListOfCurrentMonth } = useCalendar();
+    const { dateOfCalendarList, calendar } = store.state.currentMonth;
+    const weekListOfCurrentMonth = ref(calendar);
+    const variable = reactive({
+      postsQueryInput: {
+        from: store.state.currentMonth.firstDateString,
+        to: store.state.currentMonth.lastDateString,
+      },
+    });
+
+    watch(store.state.currentMonth,
+      () => {
+        weekListOfCurrentMonth.value = store.state.currentMonth.calendar;
+        variable.postsQueryInput.from = store.state.currentMonth.firstDateString;
+        variable.postsQueryInput.to = store.state.currentMonth.lastDateString;
+      });
+
+    const { result } = useQuery(gql`
+      query ($postsQueryInput: PostsQueryInput!){
+        posts(postsQueryInput: $postsQueryInput){
+          id,
+          title,
+          from,
+          to,
+          photos {
+            path,
+            caption
+          },
+        }
+      }
+    `, variable);
+    const posts = useResult(result, [] as Post[], data => data.posts as Post[]);
 
     const dayList = reactive(['日', '一', '二', '三', '四', '五', '六']);
 
     const today = new Date();
-    const todayDateOfCalendar = dateListOfCurrentMonth.value.find((dateOfCalendar) => dateOfCalendar.date?.getDate() === today.getDate());
+    const todayDateOfCalendar = dateOfCalendarList.find((dateOfCalendar) => dateOfCalendar.date?.getDate() === today.getDate());
     store.state.currentDate = todayDateOfCalendar !== undefined ? todayDateOfCalendar : new DateOfCalendar();
+
+    const getPosts = (dateOfCalendar: DateOfCalendar) => {
+      if (posts.value.length > 0) {
+        const { date } = dateOfCalendar;
+        if (date) {
+          dateOfCalendar.posts = posts.value.filter(post => {
+            const from = new Date(post.from);
+            const to = new Date(post.to);
+            return date.getTime() >= from.getTime() && date.getTime() <= to.getTime();
+          });
+        }
+      }
+
+      return dateOfCalendar;
+    };
 
     return {
       dayList,
       weekListOfCurrentMonth,
+      getPosts,
     };
   },
 });
